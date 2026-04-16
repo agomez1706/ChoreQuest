9dcae48536b3e2fe70f9e322119547d203b82a8e
George T <1fungenius@gmail.com>
2026-04-15 22:39:47 -0500

    Scaffolded Angular 21, Django, Firebase, and Bootstrap

---
+++ frontend/src/app/app.config.ts
@@ -0,0 +1,16 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideRouter(routes),
+  ],
+};
